057d086a4bdbf0136b8a9500252a0a4e6f62a85c
divi2001 <deepashok20@gmail.com>
2024-10-06 23:40:15 +0530

    Replaced build folder and changed the ip

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(
 app.use((req, res, next) => {
   res.setHeader(
     'Content-Security-Policy',
-    "default-src 'self'; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; script-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; connect-src 'self' http://localhost:3000; img-src 'self' data:;"
+    "default-src 'self'; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; script-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; connect-src 'self' https://www.shorthandonlineexam.in; img-src 'self' data:;"
   );
   next();
 });
@@ -60,7 +60,7 @@ app.use((req, res, next) => {
 app.use(cors(corsOptions));
 
 app.use(cors({
-  origin: ['*','http://3.109.1.101:3000', 'http://3.109.1.101:3001', 'http://3.109.1.101:3002', 'http://43.204.22.53:5000','http://localhost:3000'],
+  origin: ['*','http://3.109.1.101:3000', 'http://3.109.1.101:3001', 'http://3.109.1.101:3002', 'http://43.204.22.53:5000','https://www.shorthandonlineexam.in'],
   credentials: true
 }));
 
@@ -107,6 +107,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on http://localhost:3000`);
+    console.log(`Server running on https://www.shorthandonlineexam.in`);
   });
   
