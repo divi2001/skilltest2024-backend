034709209037a15afeafd4f0de79a49f1d6054ca
tanuj1221 <tcchavan999@gmail.com>
2024-10-07 10:40:56 +0530

    solving src issue

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(
 app.use((req, res, next) => {
   res.setHeader(
     'Content-Security-Policy',
-    "default-src 'self'; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; script-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; connect-src 'self' https://www.shorthandonlineexam.in http://3.6.86.1:5000; img-src 'self' data:;"
+    "default-src 'self'; style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; script-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com; connect-src 'self' https://www.shorthandonlineexam.in http://3.6.86.1:5000 http://3.6.86.1:5000/compare; img-src 'self' data:;"
   );
   next();
 });
@@ -60,7 +60,7 @@ app.use((req, res, next) => {
 app.use(cors(corsOptions));
 
 app.use(cors({
-  origin: ['*', 'http://3.109.1.101:3000', 'http://3.109.1.101:3001', 'http://3.109.1.101:3002', 'http://43.204.22.53:5000', 'https://www.shorthandonlineexam.in', 'http://3.6.86.1:5000'],
+  origin: ['*', 'http://3.109.1.101:3000', 'http://3.109.1.101:3001', 'http://3.109.1.101:3002', 'http://43.204.22.53:5000', 'https://www.shorthandonlineexam.in', 'http://3.6.86.1:5000','http://3.6.86.1:5000/compare'],
   credentials: true
 }));
 
