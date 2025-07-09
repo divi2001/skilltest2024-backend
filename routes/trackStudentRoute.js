c099026d6702d940886e4c586d2bffa69ba04586
tanuj1221 <75058861+tanuj1221@users.noreply.github.com>
2025-07-09 15:51:50 +0530

    controller batch added

--- routes/trackStudentRoute.js
+++ routes/trackStudentRoute.js
@@ -9,5 +9,6 @@ router.post('/track-students-on-exam-center-code', trackStudentController.getStu
 router.post('/track-students-on-exam-center-code/:batchNo', trackStudentController.getStudentsTrack);
 router.post('/examStage', trackStudentController.storeExamStage);
 router.get('/get-examStages', trackStudentController.getStoredStages);
+router.get('/get-active-departments', trackStudentController.getActiveDepartments);
 
 module.exports = router;
