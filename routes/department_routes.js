da63f183917ce716999a27bcab063ebb5d7f7dad
Divyang <deepashok20@gmail.com>
2025-07-21 13:09:11 +0530

    Updated expertreviewlog

--- routes/department_routes.js
+++ routes/department_routes.js
@@ -1,3 +1,4 @@
+// routes\department_routes.js
 const { departementLogin , getStudentsTrackDepartmentwise ,getCurrentStudentDetailsCenterwise,getDepartmentswithstudents ,updateDepartmentStatus, getDepartmentDetails } = require("../controllers/department/departmentController");
 
 const router = require("express").Router();
@@ -6,7 +7,7 @@ router.post("/department-login",departementLogin);
 router.post("/track-students-on-department-code",getStudentsTrackDepartmentwise);
 router.post("/update-department-status",updateDepartmentStatus);
 router.get("/get-department-batch-student-count",getCurrentStudentDetailsCenterwise);
-router.get("/get-departments",getDepartmentswithstudents);
+router.get("/get-departments-students",getDepartmentswithstudents);
 router.get("/get-department-details",getDepartmentDetails);
 
 module.exports = router;
