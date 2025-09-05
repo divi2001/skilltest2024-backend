23b582057bfbfcb0064af14897e7ab5a87edd577
Divyang <deepashok20@gmail.com>
2025-09-05 10:54:14 +0530

    Preparing for the new deployment

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(PORT, 'localhost', () => {
+app.listen(PORT, '0.0.0.0', () => {
   // console.log(`Server running on www.shorthandonlineexam.in`);
   console.log(`Server running on http://localhost:${PORT}`);
 });
